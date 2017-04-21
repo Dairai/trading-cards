f7f98c5296d813b557c14e4068f660a8710d0f3a
Dairai <dnyabando@g.harvard.edu>
2017-04-21 12:04:36 -0400

    Initial units tests for domains

--- src/test/groovy/com/cards/CardSpec.groovy
+++ src/test/groovy/com/cards/CardSpec.groovy
@@ -15,8 +15,24 @@ class CardSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test for invalid Card"() {
+        when:"new card is created with no number"
+            Card newCard = new Card()
+
+        then:"card is invalid"
+            !newCard.validate()
+
+        when:"new card is created with a number less than 0"
+            newCard = new Card(number: -1)
+
+        then:"Card is invalid"
+            !newCard.validate()
+
+        when:"new card is created with a valid number"
+            CardSet testSet = new CardSet()
+            newCard = new Card(number: 12324,set: testSet)
+
+        then:"Card is valid"
+            newCard.validate()
     }
 }
